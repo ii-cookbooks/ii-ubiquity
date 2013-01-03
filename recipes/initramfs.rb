4a99a36d961b7db32b2436199927bc7b4957a04f
Hippie Hacker <chris@hippiehacker.org>
2013-01-03 13:40:46 -0800

    add support for open and closed wifi networks in initrd stage of ubiquity

--- recipes/initramfs.rb
+++ recipes/initramfs.rb
@@ -22,13 +22,29 @@ service 'network-manager' do
 end
 
 # We could put networks in a databag
-['dd-wrt'].each do |ssid|
-  template "/etc/NetworkManager/system-connections/#{ssid}" do
-    mode 0600
-    source "nm-open-wifi.erb"
-    variables({
-      ssid: ssid
-    })
-    notifies :restart, 'service[network-manager]', :immediately
+# ['dd-wrt'].each do |ssid|
+if node['ii-ubiquity']['wifi']['open']
+  node['ii-ubiquity']['wifi']['open'].keys.each do |ssid|
+    template "/etc/NetworkManager/system-connections/#{ssid}" do
+      mode 0600
+      source "nm-open-wifi.erb"
+      variables({
+          ssid: ssid
+        })
+      notifies :restart, 'service[network-manager]' #, :immediately # this happens so early can just que it
+    end
+  end
+end
+if node['ii-ubiquity']['wifi']['wpa']
+  node['ii-ubiquity']['wifi']['wpa'].keys.each do |ssid|
+    template "/etc/NetworkManager/system-connections/#{ssid}" do
+      mode 0600
+      source "nm-closed-wifi.erb"
+      variables({
+          ssid: ssid,
+          psk: node['ii-ubiquity']['wifi']['wpa'][ssid]
+        })
+      notifies :restart, 'service[network-manager]'#, :immediately # this happens so early can just que it
+    end
   end
 end
