1779e0688a12e462eea3550294462c25ea2e135f
Hippie Hacker <chris@hippiehacker.org>
2013-01-14 09:06:41 -0800

    persistence seems to require we remove ubiquity... I'm sure this isn't right

---
+++ recipes/initramfs-persistent.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: ii-ubiquity
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "#{cookbook_name}::ohai-plugins"
+#include_recipe "#{cookbook_name}::slideshow"
+include_recipe "#{cookbook_name}::desktop"
+include_recipe "#{cookbook_name}::wireless"
+
+package 'ubiquity' do
+  action :remove
+end
+
+# need bcmwl-kernel-source on laptop
+
