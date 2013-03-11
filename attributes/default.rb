f86a74f976c2f4844a33285150c3309ed9347e86
Hippie Hacker <chris@hippiehacker.org>
2013-03-10 18:21:18 -0700

    update wifi to my new home addr

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,4 @@
-default['ii-ubiquity']['wifi']['open']['dd-wrt'] = true
+# open wifi example
+default['ii-ubiquity']['wifi']['open']['5GHz'] = true
+# wpa passworded wifi example
 default['ii-ubiquity']['wifi']['wpa']['MySecretSSID'] = 'mysecretpassword'
